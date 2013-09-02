5c2b01db06f170ce01c05e0e2124f5c7764fc58a
awkale <hi@awkale.me>
2013-09-02 09:29:30 -0400

    added function to redirect users to member page

--- wp-content/themes/brooklyn-symphony-orchestra/functions.php
+++ wp-content/themes/brooklyn-symphony-orchestra/functions.php
@@ -125,6 +125,25 @@ function magic_remove_lead_paragraph($content) {
 }
 add_filter('the_content', 'magic_remove_lead_paragraph');
 
+
+
+/**
+ * WordPress function for redirecting users on login based on user role
+ */
+function my_login_redirect( $url, $request, $user ){
+    if( $user && is_object( $user ) && is_a( $user, 'WP_User' ) ) {
+        if( $user->has_cap( 'administrator' ) ) {
+            $url = admin_url();
+        } else {
+            $url = home_url('/musicians/bso-member-page/');
+        }
+    }
+    return $url;
+}
+
+add_filter('login_redirect', 'my_login_redirect', 10, 3 );
+
+
 /*
 Style Format Options
 
